b03458b01493e266c7f566acbe39b9e13c594881
BenMortensenTTS <ttttloading@gmail.com>
2019-08-26 10:45:23 -0700

    added boxes and style

--- src/App.js
+++ src/App.js
@@ -10,9 +10,24 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>React-o-Raptor</div>
+      <div>
+        <h1>Simon</h1>
+        <Box id="1" color={{num1:255,num2:0,num3:0}} />
+        <Box id="2" color={{num1:0,num2:255,num3:0}} />
+        <Box id="3" color={{num1:0,num2:0,num3:255}} />
+        <Box id="4" color={{num1:255,num2:255,num3:0}} />
+      </div>
     )
   }
+}
+
+const Box = (props) => {
+  let boxStyle = {
+    backgroundColor: `rgba(${props.color.num1}, ${props.color.num2}, ${props.color.num3}, .4)`
+  }
+  return(
+    <div style={boxStyle} >Box {props.id}</div>
+  )
 }
 
 export default App;
